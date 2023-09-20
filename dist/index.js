37ce877fa7decff471831949739eb966e075c759
erica0321 <hyunahrho29@gmail.com>
2023-09-20 16:48:41 +0900

    Use Map

--- dist/index.js
+++ dist/index.js
@@ -2,38 +2,51 @@ import puppeteer from 'puppeteer';
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const url = 'https://www.shopcider.com/search/results?q=%ED%82%A4%EB%A7%81&key_word_type=Normal&listSource=product_list-3358;search_word;0';
+    const url = 'https://www.shopcider.com/product/list?collection_id=14&link_url=https%3A%2F%2Fwww.shopcider.com%2Fproduct%2Flist%3Fcollection_id%3D14&operationpage_title=search_result&operation_position=4-1-0&operation_type=category&operation_content=%EC%83%81%EC%9D%98&operation_image=&operation_update_time=1686838115206&listSource=search_result-%ED%82%A4%EB%A7%81%3Bcollection_14%3B4-1-0';
     const result = await extract(url, page);
+    await extract(url, page);
     console.log(JSON.stringify(result));
     console.log(`아이템 수 ${result.length}`);
+    await browser.close();
 })();
 async function getItems(page) {
-    return await page.$$eval('.product-list-item-box', async (listItem) => {
+    let elMap = new Map();
+    const ItemArray = await page.$$eval('.product-list-item-box', async (listItem) => {
         return listItem.map(item => ({
+            pdId: Number(item.querySelector('a.cider-link').href.match(/style_id=(\d+)/)[1]),
             name: item.querySelector('span.product-item-name').textContent.trim(),
             price: item
                 .querySelector('div.product-item-main-price ')
                 .textContent.trim(),
         }));
     });
+    ItemArray.map(el => {
+        elMap.set(el.pdId, { name: el.name, price: el.price });
+    });
+    return elMap;
 }
 async function extract(url, page) {
     const result = [];
+    const mapResult = new Map();
     await page.goto(url);
     let lastHeight = await page.evaluate('document.body.scrollHeight');
     while (true) {
-        await sleep(1000);
         await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-        await sleep(2000);
+        await sleep(1000);
         let newHeight = await page.evaluate('document.body.scrollHeight');
         const itemNum = await page.$$eval('.product-list-item-box', data => data.length);
-        if (newHeight == lastHeight || itemNum >= 100) {
+        if (newHeight == lastHeight || mapResult.size >= 100) {
             break;
         }
         lastHeight = newHeight;
+        const items = await getItems(page);
+        for (let [key, value] of items) {
+            mapResult.set(key, value);
+        }
     }
-    const items = await getItems(page);
-    result.push(...items);
+    for (let [key, value] of mapResult) {
+        result.push(value);
+    }
     return result.slice(0, 100);
 }
 async function sleep(ms) {
--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,46 @@ import type { Result } from './types'
   const page = await browser.newPage()
 
   const url =
-    'https://www.shopcider.com/search/results?q=%ED%82%A4%EB%A7%81&key_word_type=Normal&listSource=product_list-3358;search_word;0'
+    'https://www.shopcider.com/product/list?collection_id=14&link_url=https%3A%2F%2Fwww.shopcider.com%2Fproduct%2Flist%3Fcollection_id%3D14&operationpage_title=search_result&operation_position=4-1-0&operation_type=category&operation_content=%EC%83%81%EC%9D%98&operation_image=&operation_update_time=1686838115206&listSource=search_result-%ED%82%A4%EB%A7%81%3Bcollection_14%3B4-1-0'
 
   const result: Result = await extract(url, page)
+  await extract(url, page)
 
   console.log(JSON.stringify(result))
   console.log(`아이템 수 ${result.length}`)
 
-  // await browser.close()
+  await browser.close()
 })()
 
 //한페이지내 item 리스트 반환
 async function getItems(page: Page) {
-  return await page.$$eval('.product-list-item-box', async listItem => {
-    return listItem.map(item => ({
-      // image: item.querySelector('img.first-image').src,
-      name: item.querySelector('span.product-item-name').textContent.trim(),
-      price: item
-        .querySelector('div.product-item-main-price ')
-        .textContent.trim(),
-    }))
+  let elMap = new Map()
+  const ItemArray = await page.$$eval(
+    '.product-list-item-box',
+    async listItem => {
+      return listItem.map(item => ({
+        // image: item.querySelector('img.first-image').src,
+        pdId: Number(
+          (item.querySelector('a.cider-link') as HTMLLinkElement).href.match(
+            /style_id=(\d+)/
+          )[1]
+        ),
+        name: item.querySelector('span.product-item-name').textContent.trim(),
+        price: item
+          .querySelector('div.product-item-main-price ')
+          .textContent.trim(),
+      }))
+    }
+  )
+  ItemArray.map(el => {
+    elMap.set(el.pdId, { name: el.name, price: el.price })
   })
+  return elMap
 }
 
 async function extract(url: string, page: Page) {
   const result: Result = []
+  const mapResult = new Map()
   await page.goto(url)
   let lastHeight = await page.evaluate('document.body.scrollHeight')
   while (true) {
@@ -43,15 +58,20 @@ async function extract(url: string, page: Page) {
       data => data.length
     )
     //상품 개수가 100개 넘거나, 스크롤이 끝날때
-    if (newHeight == lastHeight || itemNum >= 100) {
+    if (newHeight == lastHeight || mapResult.size >= 100) {
       break
     }
-    
     lastHeight = newHeight
-  }
-  const items = await getItems(page)
-  result.push(...items)
 
+    // Map 형태인 items
+    const items = await getItems(page)
+    for (let [key, value] of items) {
+      mapResult.set(key, value)
+    }
+  }
+  for (let [key, value] of mapResult) {
+    result.push(value)
+  }
   return result.slice(0, 100)
 }
 
